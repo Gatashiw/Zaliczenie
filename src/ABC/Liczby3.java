cac9092d32ceb1b9ddeb562877d23dc80bf47f3b
Gatashiw <wwx8963@student.wsb.warszawa.pl>
2022-06-22 21:06:04 +0200

    Zmienienie wartosci 3 klasy

--- src/ABC/Liczby3.java
+++ src/ABC/Liczby3.java
@@ -4,7 +4,7 @@ public class Liczby3 {
     public static void main(String[] args)
     {
         System.out.println("Program wywietla pierwiastek kwadratowy ");
-        System.out.println("z liczby pi z dokładnoci dwóch miejsc po przecinku.");
-                System.out.printf("Sqrt(Pi) = " + "%2.2f\n", Math.sqrt(Math.PI));
+        System.out.println("z liczby pi z dokładnoci trzech miejsc po przecinku.");
+                System.out.printf("Sqrt(Pi) = " + "%3.3f\n", Math.sqrt(Math.PI));
     }
 }
