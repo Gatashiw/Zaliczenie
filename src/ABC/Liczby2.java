4315df40afa69ac38f62e8d17a588053e8e9ab88
Gatashiw <wwx8963@student.wsb.warszawa.pl>
2022-06-22 20:47:58 +0200

    Dodanie projektu

---
+++ src/ABC/Liczby2.java
@@ -0,0 +1,13 @@
+package ABC;
+
+public class Liczby2 {
+    public static void main(String[] args)
+    {
+        int a = 37;
+        int b = 11;
+        System.out.println("Program oblicza reszty z dzielenia całkowitego");
+        System.out.println("dwóch liczb całkowitych.");
+        System.out.println("Dla liczb a = " + a + ", b = " + b);
+        System.out.println(a + "%" + b + " = " + a%b + ".");
+    }
+}
