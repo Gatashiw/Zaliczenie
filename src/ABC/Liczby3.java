88261bf1bb71d6d5eaee2b954cc4c26d662c3991
Gatashiw <wwx8963@student.wsb.warszawa.pl>
2022-06-22 21:04:54 +0200

    Dodanie nowej klasy

---
+++ src/ABC/Liczby3.java
@@ -0,0 +1,10 @@
+package ABC;
+
+public class Liczby3 {
+    public static void main(String[] args)
+    {
+        System.out.println("Program wywietla pierwiastek kwadratowy ");
+        System.out.println("z liczby pi z dokładnoci dwóch miejsc po przecinku.");
+                System.out.printf("Sqrt(Pi) = " + "%2.2f\n", Math.sqrt(Math.PI));
+    }
+}
