4c9c575971b1b1b1263af3665eed08fd19ea1bf6
Gatashiw <wwx8963@student.wsb.warszawa.pl>
2022-06-22 20:50:45 +0200

    Zmienione wartości zmiennych

--- src/ABC/Liczby.java
+++ src/ABC/Liczby.java
@@ -3,8 +3,8 @@ package ABC;
 public class Liczby {
     public static void main(String[] args)
     {
-        int a = 37;
-        int b = 11;
+        int a = 40;
+        int b = 3;
         System.out.println("Program wywietla wynik dzielenia cakowitego");
         System.out.println("bez reszty dwóch liczb cakowitych.");
         System.out.println("Dla liczb a = " + a + ", b = " + b);
--- src/ABC/Liczby2.java
+++ src/ABC/Liczby2.java
@@ -3,8 +3,8 @@ package ABC;
 public class Liczby2 {
     public static void main(String[] args)
     {
-        int a = 37;
-        int b = 11;
+        int a = 20;
+        int b = 15;
         System.out.println("Program oblicza reszty z dzielenia całkowitego");
         System.out.println("dwóch liczb całkowitych.");
         System.out.println("Dla liczb a = " + a + ", b = " + b);
