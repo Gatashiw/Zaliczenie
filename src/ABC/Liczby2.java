e40a7e5597259f438590bee3ad85d37f87286f86
Gatashiw <wwx8963@student.wsb.warszawa.pl>
2022-06-22 21:06:46 +0200

    Zmienienie wartosci zmiennych w 2klasie

--- src/ABC/Liczby2.java
+++ src/ABC/Liczby2.java
@@ -3,8 +3,8 @@ package ABC;
 public class Liczby2 {
     public static void main(String[] args)
     {
-        int a = 20;
-        int b = 15;
+        int a = 8;
+        int b = 2;
         System.out.println("Program oblicza reszty z dzielenia całkowitego");
         System.out.println("dwóch liczb całkowitych.");
         System.out.println("Dla liczb a = " + a + ", b = " + b);
